e0e41d2f2e6c227ed6f53ff12f04f210f679e88b
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-05-10 12:51:36 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Button component</h1>
+        <button className="btn btn-danger">Button bootstrap</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
